7652b6658949699c2c438fed0070c8d491728600
Tomasz Borowicki <borowicki.tomasz@gmail.com>
2021-05-03 18:28:19 +0200

    Added Test Project

---
+++ Kadry.Tests/UnitTest1.cs
@@ -0,0 +1,43 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kadry.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    //[TestClass]
+    //public class ContrahentQueriesTests : BaseTestClass
+    //{
+    //    [TestMethod]
+    //    public void CheckIfGetByIdQueryRetunsOneContrahent()
+    //    {
+    //        //Assign
+    //        using var context = new SquareCatsDbContext(ContextOptions);
+    //        var query = new ContrahentGetByIdQuery { ContrahentId = TestContrahentIdAssignInTestBaseClass };
+    //        var queryhandler = new ContrahentGetByIdQueryHandler(context);
+    //        //Act
+    //        var result = queryhandler.Execute(query);
+    //        //Assert
+    //        Assert.AreEqual(1, query.RowsAffected);
+    //    }
+    //    [TestMethod]
+    //    public void CheckIfGetByNameQueryRetunsTwoContrahents()
+    //    {
+    //        //Assign
+    //        using var context = new SquareCatsDbContext(ContextOptions);
+    //        var query = new ContrahentGetByNameQuery { ContrahentName = TestContrahentNameAssignInTestBaseClass };
+    //        var queryhandler = new ContrahentGetByNameQueryHandler(context);
+    //        //Act
+    //        var result = queryhandler.Execute(query);
+    //        //Assert
+    //        Assert.AreEqual(2, query.RowsAffected);
+    //    }
+
+    //}
+}
